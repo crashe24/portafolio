b4e23ac9b77ea25c44ead6a61e2578abb8b1037b
Jorge Luis Leon <crashe24@hotmail.com>
2017-08-21 18:04:23 -0500

    Servicio de firebase listo

--- angular-html/portafolio/src/app/services/informacion.service.ts
+++ angular-html/portafolio/src/app/services/informacion.service.ts
@@ -6,15 +6,29 @@ export class InformacionService {
 
   info:any=[];
   cargada:boolean=false;
-
+  cargadaSobreNosotros:boolean=false;
+  equipo:any=[];
   constructor(private http: Http) {
-      this.http.get("assets/data/info.pagina.json")
-      .subscribe( data => {
-        this.info = data.json();
-        this.cargada = true;
-        //  console.log(data.json());
-
-      })
+      this.cargaInfo();
+      this.cargaSobreNosotros();
    }
 
+   cargaInfo(){
+     this.http.get("assets/data/info.pagina.json")
+     .subscribe( data => {
+       this.info = data.json();
+       this.cargada = true;
+       //  console.log(data.json());
+
+     })
+   }
+   cargaSobreNosotros(){
+     this.http.get("https://paginaweb-143f7.firebaseio.com/equipo.json")
+     .subscribe( data => {
+       this.equipo = data.json();
+       this.cargadaSobreNosotros = true;
+
+
+     })
+   }
 }
